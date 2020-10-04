e75fdd44656fb5d35de02e662bf81b2ad2758c22
e1fang <438283787@qq.com>
2020-10-05 00:17:42 +0800

    PureComponent

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import App from './类组件详解/shouldComponentUpdate'
+import App from './类组件详解/PureComponent'
 
 // ReactDOM.render(
 //   <React.StrictMode>
