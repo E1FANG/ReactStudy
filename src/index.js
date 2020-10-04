ad49e3894b533caa46aefb24d1eb457b0176d9a9
e1fang <438283787@qq.com>
2020-10-04 19:22:09 +0800

    组件在复杂情况下使用state

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@ import * as serviceWorker from './serviceWorker';
 import Father from './函数组件与类组件demo1'
 import Father2 from './函数组件与类组件使用props'
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <App /> */}
+//     <Father />
+//     <Father2 />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Father />
-    <Father2 />
-  </React.StrictMode>,
+    <Father />,
   document.getElementById('root')
 );
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/函数组件与类组件demo1.js
+++ src/函数组件与类组件demo1.js
@@ -17,7 +17,18 @@ class Son extends React.Component {
         }
     }
     add() {
-        this.setState({ n: this.state.n + 1 })
+        // this.state.n += 1   //为什么不行,
+                               //与vue的区别，vue会监听n的变化然后去刷新UI(劫持n)
+                               //但是react并没有帮你做这件事(react不知道n变化了，所以不会更新视图)
+        // this.setState(this.state)   //可以生效 但是不建议
+
+        // this.setState({ n: this.state.n + 1 })
+        // console.log(this.state.n) // 0
+         this.setState((state)=>{
+             const n = state.n+1
+             console.log(n)
+             return {n}
+         })
     }
     render() {
         return (
